fc76192b0a1cf0249e0c0e2f04d2ddcecdca98c4
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-08-09 17:56:16 +0530

    All about inbuilt functions

--- inbuilt_functions.js
+++ inbuilt_functions.js
@@ -72,6 +72,21 @@ let arr = [10,2,3,7,1];
 
 myInclude(arr,10)
 
+function myJoin(arr,basis){
+    let res ="";
+    for(let i = 0; i < arr.length; i++){
+        if(i === arr.length-1){
+            res+=arr[i];
+        }
+        else{
+            res+=arr[i]+basis
+        }
+}
+console.log(res);
+}
+
+// myJoin([1,2,3,4,5],"-")
+myJoin([1,2,3,4,5],"+")
 
 
 
